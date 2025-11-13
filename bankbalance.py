65fcffe138e610d9968d435a37fd8094e0161099
Prashantism12 <pashu.s.madival@gmail.com>
2025-11-13 17:36:48 +0530

    Update bankbalance.py

--- bankbalance.py
+++ bankbalance.py
@@ -6,5 +6,5 @@ deposit_amount = 250
 updated_balance = initial_balance + deposit_amount
 
 print(f"INITIAL BALANCE :rs{initial_balance}")
-print(f"DEPOSITE AMOUNT :rs{deposite_amount}")
+print(f"DEPOSITE AMOUNT :rs{deposit_amount}")
 print(f"updated balance:rs{updated_balance}")
